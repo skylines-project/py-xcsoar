f16bbe5b50550c5c30c8100de0cfdf04e4224f61
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-02-14 23:39:41 +0100

    setup.py: Use `print()` function

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import os
 from sys import platform
 from setuptools import setup
@@ -29,7 +31,7 @@ class XCSoarBuild(build):
         try:
             cmd.append('-j%d' % cpu_count())
         except NotImplementedError:
-            print 'Unable to determine number of CPUs. Using single threaded make.'
+            print('Unable to determine number of CPUs. Using single threaded make.')
 
         options = [
             'DEBUG=n',
